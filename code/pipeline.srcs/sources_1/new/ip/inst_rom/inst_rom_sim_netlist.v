c2ee56891983bdfb4a6f038fec5a6a3aad8aad81
pfcs33 <aaapfcs@gmail.com>
2023-09-05 00:44:17 +0800

    数码管例子 上板完成

--- code/pipeline.srcs/sources_1/new/ip/inst_rom/inst_rom_sim_netlist.v
+++ code/pipeline.srcs/sources_1/new/ip/inst_rom/inst_rom_sim_netlist.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-// Date        : Mon Sep  4 23:00:10 2023
+// Date        : Tue Sep  5 00:35:22 2023
 // Host        : DESKTOP-MPMJC1F running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode funcsim
 //               C:/Users/11731/Desktop/miniTerm/myPipeline/code/pipeline.srcs/sources_1/new/ip/inst_rom/inst_rom_sim_netlist.v
